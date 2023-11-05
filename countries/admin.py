564cb70679fe373b4c68f5c8adf3ca0d4a3019e2
Ramesh Kumar <rameshkumar.exe@gmail.com>
2023-11-05 23:36:38 +0530

    feat:Register models in admin

--- countries/admin.py
+++ countries/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from countries.models import Country, Currency
 
-# Register your models here.
+
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ("common_name",)
+    search_fields = ("common_name",)
+
+
+admin.site.register(Country, CountryAdmin)
+
+
+class CurrencyAdmin(admin.ModelAdmin):
+    list_display = ("code",)
+    search_fields = ("code",)
+
+
+admin.site.register(Currency, CurrencyAdmin)
