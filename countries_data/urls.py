7c05e05ecd531a22f141132d8f137d3e1610418f
Ramesh kumar <rameshkumar.exe@gmail.com>
2023-11-05 21:16:48 +0530

    fix: Update poject base name

--- countries_data/urls.py
+++ countries_data/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for countries_api project.
+URL configuration for countries_data project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
